e92da924f2e78d0eddfe87c0c7cf512dac7e1968
fjodor-rybakov <fedryb@mail.ru>
2019-04-11 00:05:58 +0300

    add database

--- src/Backend/Program.cs
+++ src/Backend/Program.cs
@@ -19,7 +19,6 @@ namespace Backend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:3001");
+                .UseStartup<Startup>();
     }
 }
--- src/Frontend/Program.cs
+++ src/Frontend/Program.cs
@@ -19,7 +19,6 @@ namespace Frontend
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseUrls("http://0.0.0.0:3000");
+                .UseStartup<Startup>();
     }
 }
