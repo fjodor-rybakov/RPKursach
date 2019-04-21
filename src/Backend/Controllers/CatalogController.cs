7675277633e5e0792a5bc997ef5d82ba7f153c89
fjodor-rybakov <fedryb@mail.ru>
2019-04-21 13:19:52 +0300

    Update CatalogController.cs

--- src/Backend/Controllers/CatalogController.cs
+++ src/Backend/Controllers/CatalogController.cs
@@ -16,17 +16,6 @@ namespace Backend.Controllers
         {
             using (ApplicationContext db = new ApplicationContext())
             {
-                /*var products = db.Products
-                    .Join(db.Companies,
-                        p => p.CompanyId,
-                        c => c.Id,
-                        (p, c) => new
-                        {
-                            p.ProductName,
-                            c.CompanyName,
-                            p.Price
-                        })
-                    .ToList();*/
                 var products = (from product in db.Products
                     join company in db.Companies on product.CompanyId equals company.Id
                     join category in db.Categories on product.CategoryId equals category.Id
