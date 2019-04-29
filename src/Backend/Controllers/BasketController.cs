9da2eab81de2218d1173006afa94e63f4a8ac1aa
fjodor-rybakov <fedryb@mail.ru>
2019-04-29 19:33:33 +0300

    add get del basket products

---
+++ src/Backend/Controllers/BasketController.cs
@@ -0,0 +1,106 @@
+using System;
+using System.Linq;
+using System.Security.Claims;
+using ApiErrors;
+using EntityDatabase;
+using EntityDatabase.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly ApiError _apiError = new ApiError();
+
+        [Authorize(Roles = "Покупатель")]
+        [HttpGet("{id}")]
+        public ActionResult<string> AddBasket(int id)
+        {
+            try
+            {
+                var user = GetUser(HttpContext.User);
+                if (user == null) return _apiError.UserNotFount;
+
+                var db = new ApplicationContext();
+                db.UserProducts.Add(new UserProduct
+                {
+                    ProductId = id,
+                    UserId = user.Id
+                });
+                db.SaveChanges();
+
+                return Ok(new {Message = "Продукт успещно добавлен в конзину"});
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return _apiError.ServerError;
+            }
+        }
+
+        [Authorize(Roles = "Покупатель")]
+        [HttpGet("products")]
+        public ActionResult<string> GetAllBasketProducts()
+        {
+            try
+            {
+                var user = GetUser(HttpContext.User);
+                if (user == null) return _apiError.UserNotFount;
+                
+                var db = new ApplicationContext();
+                var products =  (from userProduct in db.UserProducts 
+                    join product in db.Products on userProduct.ProductId equals product.Id
+                    where userProduct.UserId == user.Id
+                    select new
+                    {
+                        product.ProductName,
+                        product.Price
+                    }).ToList();
+
+                return Ok(products);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return _apiError.ServerError;
+            }
+        }
+
+        [Authorize(Roles = "Покупатель")]
+        [HttpDelete("{id}")]
+        public ActionResult<string> DeleteBasketProduct(int id)
+        {
+            try
+            {
+                var user = GetUser(HttpContext.User);
+                if (user == null) return _apiError.UserNotFount;
+
+                var db = new ApplicationContext();
+                var userProducts = db.UserProducts.FirstOrDefault(up => up.UserId == user.Id && up.ProductId == id);
+                if (userProducts == null) return _apiError.ProductNotFound;
+                db.UserProducts.Remove(userProducts);
+                db.SaveChanges();
+                
+                return Ok(new {Message = "Продукт успешно удалён из корзины"});
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return _apiError.ServerError;
+            }
+        }
+        
+        private static User GetUser(ClaimsPrincipal principal)
+        {
+            using (var db = new ApplicationContext())
+            {
+                var email = principal.Claims?.First(x => x.Type == ClaimTypes.Name).Value;
+                return db.Users.FirstOrDefault(u => u.Email == email);
+            }
+        }
+    }
+}
--- src/Library/EntityDatabase/Models/UserProduct.cs
+++ src/Library/EntityDatabase/Models/UserProduct.cs
@@ -3,9 +3,9 @@ namespace EntityDatabase.Models
     public class UserProduct
     {
         public int Id { get; set; }
-        
+        public int ProductId { get; set; }
         public Product Product { get; set; }
-        
+        public int UserId { get; set; }
         public User User { get; set; }
     }
 }
