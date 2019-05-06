46aa080bee021f971e6f2689e50a23b2d2fd4c54
fjodor-rybakov <fedryb@mail.ru>
2019-05-06 18:38:29 +0300

    rebase scripts

--- scripts/win/build.bat
+++ scripts/win/build.bat
@@ -2,8 +2,9 @@
 setlocal enableextensions enabledelayedexpansion
 set version=%~1
 cd ..
+cd ..
 set work_dir=%cd%
-set scripts=%cd%/scripts
+set scripts=%cd%/scripts/win
 
 set path_build=%work_dir%\build
 set path_backend=%work_dir%\src\Backend
@@ -13,22 +14,25 @@ set path_backend_publish=%path_backend%\bin\Debug\netcoreapp2.2\publish
 set path_auth_publish=%path_auth%\bin\Debug\netcoreapp2.2\publish
 
 if not defined version (
+	cd %scripts%
 	echo Version are required.
 	exit /B 1
 )
+
 if not exist %path_build% mkdir build
 cd build
+
 if exist %version% (
-	cd..
+	cd %scripts%
 	echo This version already exists.
 	exit /B 1
 )
+
 mkdir %version%
 cd %version%
 mkdir files\products
 mkdir src
 cd src
-
 mkdir Components
 
 cd %path_backend%
